d65b0c67a17bec44e724386890c737faa3fe9b86
StanislavSemashko <43435900+StanislavSemashko@users.noreply.github.com>
2020-05-25 14:51:08 +0200

    Update GermanStyleDateChecker.java
    6 out of 11 working

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
-    public static Matcher getMatcher(String dateString) {
-        return null;
+    public static Matcher getMatcher(String dateString)
+    {
+        final String longDate = "(([0-2][0-9]|3[0-1])(\\.|\\-)(0[0-9]|1[0-2])(\\.|\\-)\\d{4})";
+        final String shortDate ="\\d(\\.|-|\\s)\\d(\\.|-|\\s)(\\d{4}|\\d{2})";
+        final String regex = "(" + longDate + ")|" + "(" + shortDate + ")";
+        Matcher matcher;
+
+        Pattern pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(dateString);
+        return matcher;
     }
 }
